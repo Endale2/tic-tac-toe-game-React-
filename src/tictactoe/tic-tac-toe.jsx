cf6e53dba3f404fb20633121075f3711f6f4ce4d
Endale2 <endale406@gmail.com>
2024-08-20 22:05:32 +0300

    final commit

--- src/tictactoe/tic-tac-toe.jsx
+++ src/tictactoe/tic-tac-toe.jsx
@@ -1,53 +1,72 @@
-import React, { useState } from 'react'
-import "./TicTacToe.css"
-import O from "../assets/O.png"
-import X from "../assets/X.png"
+import React, { useState } from 'react';
+import './TicTacToe.css';
+import O from '../assets/O.png';
+import X from '../assets/X.png';
 
-let data=["","","","","","","","",""]
 function TicTacToe() {
+  const initialBoard = ["", "", "", "", "", "", "", "", ""];
+  const [board, setBoard] = useState(initialBoard);
+  const [count, setCount] = useState(0);
+  const [lock, setLock] = useState(false);
+  const [winner, setWinner] = useState(null);
 
-  let [count , setCount]=useState(0);
-  let [lock, setLock]=useState(false);
-  const toggle=(e,num)=>{
-    if (lock){
-      return 0
+  const toggle = (index) => {
+    if (lock || board[index]) return;
+    const newBoard = [...board];
+    newBoard[index] = count % 2 === 0 ? "x" : "o";
+    setBoard(newBoard);
+    setCount(count + 1);
+    checkWin(newBoard);
+  };
+
+  const checkWin = (currentBoard) => {
+    const winPatterns = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (let pattern of winPatterns) {
+      const [a, b, c] = pattern;
+      if (currentBoard[a] && currentBoard[a] === currentBoard[b] && currentBoard[a] === currentBoard[c]) {
+        setWinner(currentBoard[a]);
+        setLock(true);
+        return;
+      }
     }
-    if (count%2===0){
-      e.target.innerHTML=`<img src='${X}'>`;
-      data[num]="x";
-      setCount(++count);
+
+    if (currentBoard.every(cell => cell)) {
+      setWinner("tie");
+      setLock(true);
     }
-    else{
-      e.target.innerHTML=`<img src='${O}'>`;
-      data[num]="o";
-      setCount(++count);
-    }
-  }
+  };
+
+  const resetGame = () => {
+    setBoard(initialBoard);
+    setCount(0);
+    setLock(false);
+    setWinner(null);
+  };
 
   return (
     <div className="container">
-      <div className="title">Tic Tac Toe<span>by React</span></div>
+      <div className="title">Tic Tac Toe <span>by React</span></div>
       <div className="game-board">
-        <div className="row1">
-          <div className="boxes" onClick={(e)=>{toggle(e,0)}}></div>
-          <div className="boxes" onClick={(e)=>{toggle(e,1)}}></div>
-          <div className="boxes" onClick={(e)=>{toggle(e,2)}}></div>
-        </div>
-        <div className="row2">
-          <div className="boxes" onClick={(e)=>{toggle(e,3)}}></div>
-          <div className="boxes" onClick={(e)=>{toggle(e,4)}}></div>
-          <div className="boxes" onClick={(e)=>{toggle(e,5)}}></div>
-        </div>
-        <div className="row3">
-          <div className="boxes" onClick={(e)=>{toggle(e,6)}}></div>
-          <div className="boxes" onClick={(e)=>{toggle(e,7)}}></div>
-          <div className="boxes" onClick={(e)=>{toggle(e,8)}}></div>
-        </div>
-        
+        {board.map((cell, index) => (
+          <div key={index} className="boxes" onClick={() => toggle(index)}>
+            {cell && <img src={cell === "x" ? X : O} alt={cell} />}
+          </div>
+        ))}
       </div>
-      <button className="reset">Reset</button>
+      {winner && <div className="result">{winner === "tie" ? "It's a Tie!" : `${winner === "x" ? "X" : "O"} Wins!`}</div>}
+      <button className="reset" onClick={resetGame}>Reset</button>
     </div>
-  )
+  );
 }
 
-export default TicTacToe
+export default TicTacToe;
